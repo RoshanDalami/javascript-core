6432715b08260108d022be53a3b42621ec0e7cf0
RoshanDalami <roshandalami0@gmail.com>
2023-08-20 15:38:16 +0545

    initial commit

---
+++ Basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all javascript code as newer version code .
+
+//alert(3 + 3); //alert can be use like this only on browser not in node environment
+//code should be readable -> code readability should be high
+
+// Datatype in javascript
+
+//number => 2 to the power 53
+// bigint //probably will never use
+//string => '' anyting inside single quote or double quote or inside string letrals
+// null => also a standalonevalue or also a datatype
+//undefine => value hasnot been assigned.
+//symbol => for unique
+//object
+
+console.log(typeof "roshan");
